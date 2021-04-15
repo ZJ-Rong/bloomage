1dfe8374cded8bdc8ae1feee42298ef5a1a9e72d
ZJ_Rong <qq307365873@gmail.com>
2021-04-15 15:23:33 +0800

    测试

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        var_dump(123);exit;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/',[\App\Http\Controllers\UserController::class,'index']);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
